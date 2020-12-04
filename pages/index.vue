d89b72c8e0441218fc4119c1c34142076476adef
switzerchees <patrickmichel96@yahoo.de>
2020-12-04 11:57:02 +0100

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="container">
+    <div>
+      <Logo />
+      <h1 class="title">test-nuxt-version</h1>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--green"
+        >
+          Documentation
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--grey animate-ping"
+          @click="incCount"
+        >
+          GitHub {{ count }}
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { ref, onMounted } from '@nuxtjs/composition-api'
+
+export default Vue.extend({
+  setup() {
+    const count = ref(0)
+    const incCount = () => (count.value += 1)
+
+    onMounted(() => {
+      count.value = 10
+    })
+
+    return {
+      count,
+      incCount,
+    }
+  },
+})
+</script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+@apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
